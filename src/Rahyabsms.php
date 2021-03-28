e39830f6befea1dcf7087c1d689f07774f2b3398
AmirAghaee <amir.aghaee73@gmail.com>
2021-03-28 14:54:55 +0430

    Update Rahyabsms.php

--- src/Rahyabsms.php
+++ src/Rahyabsms.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Facade;
  * Class Rahyabsms
  *
  * @method static string send(string $mobile, string $message, string $recId = null)
+ * @method static string sendAll(array $mobiles, string $message, string $recId = null)
  * @method static string getCredit()
  * @method static string GetExpireDate()
  *
